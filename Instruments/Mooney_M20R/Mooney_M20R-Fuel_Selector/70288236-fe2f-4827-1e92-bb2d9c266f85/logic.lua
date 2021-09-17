8daeb38fc5f736f28e2e0cd71af5ec39175fbcba
Crunchmeister1 <75218511+Crunchmeister1@users.noreply.github.com>
2021-09-17 18:57:43 -0400

    Mooney M20
    Added fuel selector

---
+++ Instruments/Mooney_M20R/Mooney_M20R-Fuel_Selector/70288236-fe2f-4827-1e92-bb2d9c266f85/logic.lua
@@ -0,0 +1,32 @@
+--add graphics
+img_add_fullscreen("FuelSelectPanel.png")
+img_knob = img_add("FuelSelectKnob.png", 38, 45, 305, 305)
+
+-- set functions to switch tanks
+function callback_left()
+    fs2020_event("FUEL_SELECTOR_LEFT")
+end    
+
+function callback_right()
+    fs2020_event("FUEL_SELECTOR_RIGHT")
+end
+
+function callback_off()
+    fs2020_event("FUEL_SELECTOR_OFF")
+end
+-- set touch zones for switch activation
+button_off = button_add(nil,nil,60,280,150,150,callback_off)
+button_left = button_add(nil,nil,60,30,150,150,callback_left)
+button_right = button_add(nil,nil,260,30,150,150,callback_right)
+
+function new_pos_select(tank)
+    if (tank == 2) then -- left tank
+        rotate(img_knob, 103)
+    elseif (tank == 3) then -- right tank
+        rotate(img_knob, 185)
+    elseif (tank == 0) then -- fuel off
+        rotate(img_knob, -5)
+    end
+end
+
+fs2020_variable_subscribe("RECIP ENG FUEL TANK SELECTOR:1", "enum", new_pos_select)
