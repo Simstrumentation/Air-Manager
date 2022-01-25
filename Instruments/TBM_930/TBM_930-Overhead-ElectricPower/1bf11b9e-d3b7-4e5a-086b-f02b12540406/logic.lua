383093c68aef77dacd72e0dfd3421294d10b4987
rob2791 <38576265+rob2791@users.noreply.github.com>
2022-01-24 22:39:15 -0500

    Update TBM Readme & ElectricPower Bug

--- Instruments/TBM_930/TBM_930-Overhead-ElectricPower/1bf11b9e-d3b7-4e5a-086b-f02b12540406/logic.lua
+++ Instruments/TBM_930/TBM_930-Overhead-ElectricPower/1bf11b9e-d3b7-4e5a-086b-f02b12540406/logic.lua
@@ -8,7 +8,7 @@
    
 - **v1.0**  01-21-2022 SIMSTRUMENTATION
     - Original Panel Created
-- **v1.0**  01-24-2022 
+- **v1.1**  01-24-2022 
     - Commented out all subscribes that are not working as it's causing a CTD when MIXMUGZ MOD is installed.
 
 ## Left To Do:
