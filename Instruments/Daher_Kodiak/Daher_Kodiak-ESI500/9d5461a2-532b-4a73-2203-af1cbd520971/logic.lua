81a31e791101b573d307723e39c239a78706c111
Crunchmeister1 <75218511+Crunchmeister1@users.noreply.github.com>
2022-04-02 00:12:57 -0400

    Kodiak
    ESI500 update v1.01

--- Instruments/Daher_Kodiak/Daher_Kodiak-ESI500/9d5461a2-532b-4a73-2203-af1cbd520971/logic.lua
+++ Instruments/Daher_Kodiak/Daher_Kodiak-ESI500/9d5461a2-532b-4a73-2203-af1cbd520971/logic.lua
@@ -8,6 +8,10 @@
 
     ESI500 Backup Flight Display Overlay
     
+    V1.01 - Released 2022-01-04
+        - Fixed incorrect behaviour on knob
+        - Clicking knob resets altimeter to std when menu not showing
+        
     V1.0 - Released 2022-31-03
            
     KNOWN ISSUES:
@@ -21,13 +25,16 @@ local tgl_menu
 
 --Background graphic
 img_add_fullscreen("bg.png")
-= 0
+
 -- menu button
+-- Menu button uses an lvar that is latched at 1 when the menu button is pressed. This will display
+-- the menu in the ESI500. Lvar much be toggled back to 0 to close it
+
 function menu_cb(pos)
-    if tgl_menu == 1 then
-        fs2020_variable_write("L:SWS_ESI500_Menu_Level", "Number", 0 )
-     else
-         fs2020_variable_write("L:SWS_ESI500_Menu_Level", "Number", 1 )
+    if tgl_menu == 0 or tgl_menu == 2 then
+        fs2020_variable_write("L:SWS_ESI500_Menu_Level", "Number", 1 )
+     elseif tgl_menu == 1 then
+         fs2020_variable_write("L:SWS_ESI500_Menu_Level", "Number", 0 )
      end     
 end
 menu_id = button_add("btn_menu_rest.png", "btn_menu_pressed.png", 89, 390, 79, 48, menu_cb)
@@ -38,19 +45,26 @@ end
 fs2020_variable_subscribe("L:SWS_ESI500_Menu_Level", "Number", new_menu_pos)
 
 --Knob
-
 function rotate_knob(dir)
-    fs2020_variable_write("L:SWS_ESI500_Menu_Option_Change", "Number", dir)
-    function timer_callback(count)
-         fs2020_variable_write("L:SWS_ESI500_Menu_Option_Change", "Number", 0)
+    if tgl_menu ==0 then
+         if dir == 1 then
+                fs2020_event("K:Kohlsman_Inc",3)
+           elseif dir == -1 then
+                fs2020_event("K:Kohlsman_Dec",3)
+           end
+        else
+        fs2020_variable_write("L:SWS_ESI500_Menu_Option_Change", "Number", dir)
+        function timer_callback(count)
+            fs2020_variable_write("L:SWS_ESI500_Menu_Option_Change", "Number", 0)
+        end
+        timer_start(50, timer_callback)  
     end
-    timer_start(50, timer_callback)  
 end
 
-function knob_turn_cb(direction)
-    if direction == 1 then
+function knob_turn_cb(dir)
+    if dir == 1 then
             rotate_knob(1)        
-    elseif direction == -1 then
+    elseif dir == -1 then
         rotate_knob(-1)
     end
 end
@@ -59,10 +73,15 @@ img_add("knob_highlight.png", 338, 365, 102, 102)
 
 
 function knob_click_cb()
-     fs2020_variable_write("L:SWS_ESI500_Menu_Select", "Number", 1)
-     function timer_click_callback(count)
-             fs2020_variable_write("L:SWS_ESI500_Menu_Select", "Number", 0)
-     end
-      timer_start(50, timer_click_callback) 
+    if tgl_menu == 0 then
+        fs2020_event("K:BAROMETRIC_STD_PRESSURE", 3)
+    else 
+        fs2020_variable_write("L:SWS_ESI500_Menu_Select", "Number", 1)
+        function timer_click_callback(count)
+                fs2020_variable_write("L:SWS_ESI500_Menu_Select", "Number", 0)
+        end
+        timer_start(50, timer_click_callback) 
+                
+    end
 end
 button_add(nil, nil, 358, 382, 74, 74, knob_click_cb)
