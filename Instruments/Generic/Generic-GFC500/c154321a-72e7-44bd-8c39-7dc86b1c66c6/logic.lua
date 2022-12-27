a6ad9a372001ce66f2407ec8bd1a2f1fd1c1219e
Crunchmeister1 <75218511+Crunchmeister1@users.noreply.github.com>
2022-12-27 12:10:03 -0500

    GRC500
    1.12 - graphics update

--- Instruments/Generic/Generic-GFC500/c154321a-72e7-44bd-8c39-7dc86b1c66c6/logic.lua
+++ Instruments/Generic/Generic-GFC500/c154321a-72e7-44bd-8c39-7dc86b1c66c6/logic.lua
@@ -15,7 +15,8 @@ Version info:
     - Added backlighting
     - Added optional sounds
 - **v1.0** - 2022-12-11
-
+    - Original Release
+    
 NOTES: 
 - Should work in most aircraft with a GFC500 or similar autopilot. 
 - Yaw Damper button can be hidden or shown to match your plane's AP module via user property
